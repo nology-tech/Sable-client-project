229fe6eb19c7477762f0113c7f4dc7e26b941cb1
Hannah <hannahamitchell99@gmail.com>
2023-02-20 15:03:36 +0200

    changed items

---
+++ src/components/NavItem/NavItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../NavItem/NavItem.scss'
+
+const NavItem = () => {
+  return (
+    <div className='nav-item'>
+        <div className='nav-item__image'>Image</div>
+        <div className='nav-item__text'>Text</div>
+    </div>
+  )
+}
+
+export default NavItem
--- src/containers/NavContainer/NavContainer.jsx
+++ src/containers/NavContainer/NavContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import NavItem from '../../components/NavItem/NavItem'
 import "./NavContainer.scss"
 
 
@@ -6,7 +7,13 @@ const NavContainer = () => {
   return (
     <nav className='nav-container'>
     <div className="nav-container__logo">Logo</div>
-    <div className='nav-container__menu-options'>Menu options</div>
+    <div className='nav-container__menu-options'>
+      <NavItem/>
+      <NavItem/>
+      <NavItem/>
+      <NavItem/>
+      <NavItem/>
+    </div>
     <div className="nav-container__line-break">Line break</div>
     <div className="nav-container__settings">Setting options</div>
     </nav>
