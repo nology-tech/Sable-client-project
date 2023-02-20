c5fa3200aa77ce8e0725bf00800823b6e25735ff
Hannah <hannahamitchell99@gmail.com>
2023-02-20 13:30:16 +0200

    added testing for nav container

--- src/containers/NavContainer/NavContainer.jsx
+++ src/containers/NavContainer/NavContainer.jsx
@@ -4,13 +4,12 @@ import "./NavContainer.scss"
 
 const NavContainer = () => {
   return (
-    <div className='nav-container'>
+    <nav className='nav-container'>
     <div className="nav-container__logo">Logo</div>
-    <div className="nav-container__line-break">Line break</div>
     <div className='nav-container__menu-options'>Menu options</div>
     <div className="nav-container__line-break">Line break</div>
     <div className="nav-container__settings">Setting options</div>
-    </div>
+    </nav>
   )
 }
 
