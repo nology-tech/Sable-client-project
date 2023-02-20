74fff47a4878a7450ab4501b3eebcbf4acc35cb9
Tomas Truyols <tomast25@hotmail.com>
2023-02-20 16:52:56 +0000

    feat: added compenent datacard, style: started styling

---
+++ src/components/DataCard/DataCard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./DataCard.scss";
+
+const DataCard = (props) => {
+  const {
+    name,
+    email,
+    mobileNumber,
+    isConsumer,
+    isEmployed,
+    courseName,
+    resourceName,
+    quantity,
+    cost,
+    isAutoPurchase,
+    autoPurchaseLevel,
+  } = props;
+
+  return name ? (
+    <div className="user-card">
+      <h1 className="user-card__name">{name}</h1>
+      <p className="user-card__email">{email}</p>
+      <p className="user-card__mobile-number">{mobileNumber}</p>
+      {isConsumer ? (
+        <p className="user-card__job">Consumer</p>
+      ) : (
+        <p className="user-card__job">Consultant</p>
+      )}
+      {isEmployed ? (
+        <p className="user-card__employed">YES</p>
+      ) : (
+        <p className="user-card__employed">NO</p>
+      )}
+      <p className="user-card__course-name">{courseName}</p>
+      <a className="user-card__link">{`>`}</a>
+    </div>
+  ) : (
+    <div className="resource-card">
+      <h1 className="resource-card__name">{resourceName}</h1>
+      <p className="resource-card__quantity">{quantity}</p>
+      <p className="resource-card__cost">{cost}</p>
+      {isAutoPurchase ? (
+        <p className="resource-card__auto-purchase">YES</p>
+      ) : (
+        <p className="resource-card__auto-purchase">NO</p>
+      )}
+      <p className="resource-card__level">{autoPurchaseLevel}</p>
+      <a className="resource-card__link">{`>`}</a>
+    </div>
+  );
+};
+
+export default DataCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import DataCard from "../../components/DataCard/DataCard";
 import Layout from "../../components/Layout/Layout";
 import NavContainer from "../../containers/NavContainer/NavContainer";
 import "../Home/Home.scss";
@@ -7,14 +8,22 @@ const Home = () => {
     <div className="app-container">
       <NavContainer />
       <Layout>
+        <DataCard
+          name={"Test"}
+          email={"hello"}
+          mobileNumber={2423789}
+          isConsumer={true}
+          isEmployed={false}
+          courseName={"whatever"}
+        />
         <h1>Page Heading</h1>
         <h2>Section Heading</h2>
         <h3>Panel Heading</h3>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
-          dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
-          maxime tempora, temporibus beatae voluptas repellat rerum.
-          Dignissimos, necessitatibus.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error dolor
+          in nostrum minima odio a fuga saepe soluta adipisci perspiciatis maxime
+          tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
+          necessitatibus.
         </p>
       </Layout>
     </div>
