ea6931826ebd65f21c8023a4354196a25d35947c
Josh Tucker <joshuaatucker@hotmail.com>
2023-02-20 16:11:30 +0000

    nav-item styles

--- src/components/NavItem/NavItem.jsx
+++ src/components/NavItem/NavItem.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import '../NavItem/NavItem.scss'
+import React from "react";
+import "../NavItem/NavItem.scss";
 
-const NavItem = ({image, itemText}) => {
+const NavItem = ({ image, itemText }) => {
   return (
-    <div className='nav-item'>
-        <div className='nav-item__image-container'>
-          <img className="nav-item__image" src={image} alt={itemText}/>
+    <div className="nav-item">
+      <div className="nav-item__content">
+        <div className="nav-item__image-container">
+          <img className="nav-item__image" src={image} alt={itemText} />
         </div>
-        <div className='nav-item__text'>
-            <h3>{itemText}</h3>
+        <div className="nav-item__text">
+          <p>{itemText}</p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavItem
+export default NavItem;
