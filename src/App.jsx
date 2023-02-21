da2455aa09facc1fb09feceb44723062eadf9953
bajserowiczMikolaj <mbajserowicz@gmail.com>
2023-02-21 12:12:39 +0000

    add inputs fields

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,10 @@ const App = () => {
       <Route path="*" element={<Home />}/>  
     </Routes> 
     <div className="input-field-container">
-      <InputField id="first-name" label="First Name" />
+      <InputField id="first-name" label="First Name" placeholderText="Mark" handleInput={handleName}  />
+      <InputField id="last-name" label="Last Name" placeholderText="Birchall" handleInput={handleName}  />
+      <InputField id="email" label="Email Address" placeholderText="thiel90@hotmail.com" handleInput={handlEmail}  />
+      <InputField id="number" label="Mobile Number" placeholderText="+44 1234 567890" handleInput={handlNumber}  />
     </div>
     </div>
   );
