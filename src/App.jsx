1f336011b7998b32b5f13664b64f0f33968370aa
Jina <jinayoussef@hotmail.co.uk>
2023-02-21 10:02:35 +0000

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./styles/main.scss";
+import InputField from "./components/InputField/InputField";
 
 const App = () => {
   return (
+    <div>
     <Routes>
       <Route path="*" element={<Home />} />
     </Routes>
+    <div className="input-field-container">
+      <InputField id="first-name" label="First Name" />
+    </div>
+    </div>
   );
 };
 
