6099ad5ed028f8c1059618bd5892d9ddf920e19a
Becky Rushton <beckyrushton30@gmail.com>
2023-02-21 10:17:45 +0000

    started styling employee card

--- src/components/EmployeeOverviewCard/EmployeeOverviewCard.jsx
+++ src/components/EmployeeOverviewCard/EmployeeOverviewCard.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 import "./EmployeeOverviewCard.scss";
+// import divide from "../../assets/images";
 
 const EmployeeOverviewCard = ({ startDate, courseCompletion, manager }) => {
   return (
     <div className="employee-overview-card">
-      <div className="employee-overview-card__top">
-        <h3 className="employee-overview-card__top--title">
-          Employee Overview
-        </h3>
-        <p className="employee-overview-card__top--text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-          deserunt, natus aliquid labore possimus similique molestiae architecto
-          voluptate consequatur veritatis ut, quam amet atque ducimus,
-          necessitatibus accusamus sequi recusandae perferendis?
-        </p>
-      </div>
+      <h3 className="employee-overview-card__title">Employee Overview</h3>
+      <p className="employee-overview-card__text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+        deserunt, natus aliquid labore possimus similique molestiae architecto
+        voluptate consequatur veritatis ut, quam amet atque ducimus,
+        necessitatibus accusamus sequi recusandae perferendis?
+      </p>
       <div className="employee-overview-card__bottom">
-        <p className="employee-overview-card__bottom--date">
-          Employee Start Date: {startDate}
-        </p>
-        <p className="employee-overview-card__bottom--completion">
-          Course Completion: {courseCompletion}
-        </p>
-        <p className="employee-overview-card__bottom--manager">
-          Manager: {manager}
-        </p>
+        <div className="employee-overview-card__left">
+          <p className="employee-overview-card__left--date">
+            Employee Start Date:
+          </p>
+          <p className="employee-overview-card__left--course">
+            Course Completion:
+          </p>
+          <p className="employee-overview-card__left--manager">Manager:</p>{" "}
+        </div>
+
+        <div className="employee-overview-card__right">
+          <p className="employee-overview-card__right--date">
+            {startDate} start date
+          </p>
+          <p className="employee-overview-card__right--course">
+            {courseCompletion} completion
+          </p>
+          <p className="employee-overview-card__right--manager">
+            {manager} manager
+          </p>
+        </div>
       </div>
     </div>
   );
