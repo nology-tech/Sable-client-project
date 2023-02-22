cf326ac0cc91a9b1f47608867aa93b04ad148d37
Becky Rushton <beckyrushton30@gmail.com>
2023-02-22 15:38:23 +0000

    removed testing code from home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,12 @@
 import Layout from "../../components/Layout/Layout";
-// import NavContainer from "../../containers/NavContainer/NavContainer";
+import NavContainer from "../../containers/NavContainer/NavContainer";
 import "../Home/Home.scss";
-import PageHeader from "../../components/PageHeader/PageHeader";
 
 const Home = () => {
   return (
     <div className="app-container">
-      {/* <NavContainer /> */}
+      <NavContainer />
       <Layout>
-        <PageHeader
-          heading="Home"
-          headerType="headingButtonDropdown"
-          optionsArr={[]}
-        />
-
         <h1>Page Heading</h1>
         <h2>Section Heading</h2>
         <h3>Panel Heading</h3>
