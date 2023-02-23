2c27388db1d160598a7215ff448a6e3a11c700cc
Becky Rushton <114007276+BeckyRushton@users.noreply.github.com>
2023-02-23 10:37:43 +0000

    Feature 23 page header (#23)
    * built foundation of card and started styling
    
    * added colour variables to button scss file
    
    * finalised styling and added test file to check button is rendered on card
    
    * adjusted styling so buttons will be inline
    
    * made pageheader and added styling for desktop
    
    * styled mobile version
    
    * removed accidental css files
    
    * added jsx for header
    
    * added JSX for different pages
    
    * changed code for turnary statement
    
    * added README.md with instructions on how to pass props and changed class names and structure of code
    
    * added styling to various headers in desktop version
    
    * added styling to mobile headings
    
    * removed testing code from home
    
    * made changes to styling and jsx file
    
    * made changes to styling and jsx file
    
    * adjusted styling, added some tests removed options placeholder from dropdownJSX
    
    * changed height styling added background color for mobile view
    
    ---------
    
    Co-authored-by: navina321 <81630548+navina321@users.noreply.github.com>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import Layout from "../../components/Layout/Layout";
+import PageHeader from "../../components/PageHeader/PageHeader";
 import NavContainer from "../../containers/NavContainer/NavContainer";
 import "../Home/Home.scss";
 import BookingDetailsCard from "../../components/BookingDetailsCard/BookingDetailsCard";
 import ContactDetailsCard from "../../components/ContactDetailsCard/ContactDetailsCard";
+
 const Home = () => {
   const cardObject = [{"birthDates": "10/10/2000" ,"email":"mikolaj@gmail.com" , "mobileNumber" : "07424020033","bookingDate":"20/10/2000" ,"bookingTime":"11:20","staffMember":"staff01"}]
 
@@ -10,14 +12,15 @@ const Home = () => {
     <div className="app-container">
       <NavContainer />
       <Layout>
+        <PageHeader headerType="headingOnly" heading="Home" />
         <h1>Page Heading</h1>
         <h2>Section Heading</h2>
         <h3>Panel Heading</h3>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error dolor
-          in nostrum minima odio a fuga saepe soluta adipisci perspiciatis maxime
-          tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
-          necessitatibus.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
+          dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
+          maxime tempora, temporibus beatae voluptas repellat rerum.
+          Dignissimos, necessitatibus.
         </p>
         <div>
         <BookingDetailsCard cardObject = {cardObject} />
