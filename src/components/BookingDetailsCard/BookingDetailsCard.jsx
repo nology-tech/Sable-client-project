4eabbe6380a0293d01c35516dce169978a125cd2
Jina <jinayoussef@hotmail.co.uk>
2023-02-23 13:00:13 +0000

    styling the cards

--- src/components/BookingDetailsCard/BookingDetailsCard.jsx
+++ src/components/BookingDetailsCard/BookingDetailsCard.jsx
@@ -7,15 +7,15 @@ const BookingDetailsCard = () => {
     <div className="booking-card">
       <h1 className="booking-card__heading">Booking Details</h1>
 
-      <div className="booking-card__date">
+      <div className="booking-card__section booking-card__date">
         <h2 className="booking-card__subheading"> Date:</h2>
         <p className="booking-card__data">11.11.2000</p>
       </div>
-      <div className="booking-card_time">
+      <div className="booking-card__section booking-card_time">
         <h2 className="booking-card__subheading">Time:</h2>
         <p className="booking-card__data">10;20</p>
       </div>
-      <div className="booking-card__staff">
+      <div className="booking-card__section booking-card__staff">
         <h2 className="booking-card__subheading">Staff Member:</h2>
         <p className="booking-card__data">staff 01</p>
       </div>
