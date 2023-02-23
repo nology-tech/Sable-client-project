5ebf24c043532a832573a4bd035d9234f65a43a5
Jina <jinayoussef@hotmail.co.uk>
2023-02-23 11:11:19 +0000

    removed button comments

--- src/pages/NewAppointment/NewAppointment.jsx
+++ src/pages/NewAppointment/NewAppointment.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import "./NewAppointment.scss"
 import UserInfoContainer from '../../containers/UserInfoContainer/UserInfoContainer'
 import NavContainer from '../../containers/NavContainer/NavContainer'
+import PageHeader from '../../components/PageHeader/PageHeader'
 
 const NewAppointment = () => {
   return (
     <div className="new-appointment">
         <NavContainer />
         <div className='header'>
-            Header
+            <PageHeader headerType="headingOnly" heading="Book Appointments"/>
         </div>
         <div className='info-container'>
-        <UserInfoContainer />
+          <UserInfoContainer />
         </div>
     </div>
   )
