fa3569a595d39099ba06e1284c2b4477e64c255b
Hannah Mitchell <119801701+hannahmitchell99@users.noreply.github.com>
2023-02-23 14:09:14 +0200

    Feature 58 home page (#26)
    * raw design and raw scss
    
    * adding styles-logo
    
    * firstDraftComplete
    
    * added testing for nav container
    
    * changed items
    
    * added p tag to nav item
    
    * started nav-container props
    
    * nav-item styles
    
    * styles done nav-container
    
    * added global font
    
    * added testing
    
    * uncommented * in typography
    
    * added settings and line break
    
    * pulled down changes
    
    * testing for settings
    
    * added styles and tests and updated pull origin main
    
    * added logo and changed height of menu options
    
    * added testing
    
    * added logo
    
    * amended styling to suit logo
    
    * commented back in font family
    
    * git pull oriin main
    
    * changes in nav container
    
    * updated after pull
    
    * fixed grid, added in dummy header
    
    * changed breakpoint
    
    * styling
    
    * changes to responsiveness
    
    * styles completed apart from button
    
    * added button ready for media styles
    
    * started styles for media query
    
    * added responsiveness
    
    * responsiveness
    
    * changed width
    
    * changed styling
    
    * removed whitespace
    
    * testing
    
    * removed unused imports
    
    * updated pull request
    
    * amended styles with addition of header
    
    ---------
    
    Co-authored-by: Josh Tucker <joshuaatucker@hotmail.com>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,19 @@
 import Layout from "../../components/Layout/Layout";
-import PageHeader from "../../components/PageHeader/PageHeader";
 import NavContainer from "../../containers/NavContainer/NavContainer";
 import "../Home/Home.scss";
 import BookingDetailsCard from "../../components/BookingDetailsCard/BookingDetailsCard";
 import ContactDetailsCard from "../../components/ContactDetailsCard/ContactDetailsCard";
-
+import HomePageContainer from "../../containers/HomePageContainer/HomePageContainer";
+import PageHeader from "../../components/PageHeader/PageHeader.jsx";
 const Home = () => {
   const cardObject = [{"birthDates": "10/10/2000" ,"email":"mikolaj@gmail.com" , "mobileNumber" : "07424020033","bookingDate":"20/10/2000" ,"bookingTime":"11:20","staffMember":"staff01"}]
 
   return (
-    <div className="app-container">
+    <div className="home-container">
       <NavContainer />
       <Layout>
-        <PageHeader headerType="headingOnly" heading="Home" />
-        <h1>Page Heading</h1>
-        <h2>Section Heading</h2>
-        <h3>Panel Heading</h3>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
-          dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
-          maxime tempora, temporibus beatae voluptas repellat rerum.
-          Dignissimos, necessitatibus.
-        </p>
+        <PageHeader heading="Home" headerType="headingOnly" />
+        <HomePageContainer />
         <div>
         <BookingDetailsCard cardObject = {cardObject} />
       <ContactDetailsCard cardObject = {cardObject} />
