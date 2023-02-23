1a6f7f547919eda37fa8a58b2ccb235746bc4cf4
bajserowiczMikolaj <mbajserowicz@gmail.com>
2023-02-23 12:56:51 +0000

    display cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { Route, Routes } from "react-router-dom";
+import BookingDetailsCard from "./components/BookingDetailsCard/BookingDetailsCard";
+import ContactDetailsCard from "./components/ContactDetailsCard/ContactDetailsCard";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login.jsx";
-import NewAppointment from "./pages/NewAppointment/NewAppointment";
+// import NewAppointment from "./pages/NewAppointment/NewAppointment";
 
 import "./styles/base/_reset.scss"
 import "./styles/base/_typography.scss"
 
 const App = () => {
+  const cardObject = [{birthDates: "10/10/2000" ,email:"mikolaj@gmail.com" , mobileNumber : "07424020033",bookingDate:"20/10/2000" ,bookingTime:"11:20",staffMember:"staff01"}]
+
   return (
+    <div>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/new-appointment" element = {<NewAppointment/>} />
+      {/* <Route path="/new-appointment" element = {<NewAppointment/>} /> */}
     </Routes>
+    <div>
+      <BookingDetailsCard cardObject = {cardObject} />
+      <ContactDetailsCard cardObject = {cardObject} />
+      </div>
+    </div>
   );
 };
+
+
 
 export default App;
