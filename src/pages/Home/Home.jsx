ad008f6b48a7fb3b3da65d5a9f31d387edd3abd2
bajserowiczMikolaj <mbajserowicz@gmail.com>
2023-02-23 13:10:29 +0000

    trying render page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
 import Layout from "../../components/Layout/Layout";
 import NavContainer from "../../containers/NavContainer/NavContainer";
 import "../Home/Home.scss";
+import BookingDetailsCard from "../../components/BookingDetailsCard/BookingDetailsCard";
+import ContactDetailsCard from "../../components/ContactDetailsCard/ContactDetailsCard";
 const Home = () => {
+  const cardObject = [{"birthDates": "10/10/2000" ,"email":"mikolaj@gmail.com" , "mobileNumber" : "07424020033","bookingDate":"20/10/2000" ,"bookingTime":"11:20","staffMember":"staff01"}]
+
   return (
     <div className="app-container">
       <NavContainer />
@@ -15,6 +19,10 @@ const Home = () => {
           tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
           necessitatibus.
         </p>
+        <div>
+        <BookingDetailsCard cardObject = {cardObject} />
+      <ContactDetailsCard cardObject = {cardObject} />
+      </div>
       </Layout>
     </div>
   );
