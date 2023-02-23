712cc12240aa2f63be9f5c85af96886bc9356620
Josh Tucker <joshuaatucker@hotmail.com>
2023-02-23 12:02:25 +0000

    amended styles with addition of header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,14 @@ import Layout from "../../components/Layout/Layout";
 import NavContainer from "../../containers/NavContainer/NavContainer";
 import "../Home/Home.scss";
 import HomePageContainer from "../../containers/HomePageContainer/HomePageContainer";
-
+import PageHeader from "../../components/PageHeader/PageHeader.jsx";
 const Home = () => {
   return (
     <div className="home-container">
       <NavContainer />
       <Layout>
-        <div className="header">Header</div>
-        <HomePageContainer/>
+        <PageHeader heading="Home" headerType="headingOnly" />
+        <HomePageContainer />
       </Layout>
     </div>
   );
