68e55072c85654cf16d48915d0b50815700d83c8
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-02-23 16:38:01 +0000

    removing components from Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,15 @@ import PageHeader from "../../components/PageHeader/PageHeader.jsx";
 import HomePageContainer from "../../containers/HomePageContainer/HomePageContainer";
 import NavContainer from "../../containers/NavContainer/NavContainer";
 import "../Home/Home.scss";
-import BookingDetailsCard from "../../components/BookingDetailsCard/BookingDetailsCard";
-import ContactDetailsCard from "../../components/ContactDetailsCard/ContactDetailsCard";
+
 const Home = () => {
-  const cardObject1 = {birthDate: "10/10/2000" , email:"mikolaj@gmail.com" , number: "07424020033"}
-  const cardObject2 = {date: "10/10/2022", time:"12:40", staff: "Rob"}
+  
   return (
     <div className="home-container">
       <NavContainer /> 
       <Layout>
         <PageHeader heading="Home" headerType="headingOnly" />
         <HomePageContainer />
-        <div>
-        <BookingDetailsCard cardObject={cardObject2} />
-        <ContactDetailsCard cardObject={cardObject1} />
-      </div>
       </Layout>
     </div>
   );
