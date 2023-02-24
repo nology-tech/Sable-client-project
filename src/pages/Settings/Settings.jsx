70c7e5a69cba7630ec78dea0102ab7c1faabb28c
navina321 <81630548+navina321@users.noreply.github.com>
2023-02-24 12:39:33 +0000

    moved testcode from home to settings page

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,19 +1,18 @@
 import Layout from "../../components/Layout/Layout";
-import NavContainer from "../../containers/NavContainer/NavContainer";
-import "../Home/Home.scss";
-import HomePageContainer from "../../containers/HomePageContainer/HomePageContainer";
 import PageHeader from "../../components/PageHeader/PageHeader.jsx";
+import NavContainer from "../../containers/NavContainer/NavContainer";
+import SettingsCard from "../../components/SettingsCard/SettingsCard";
+import "../Home/Home.scss";
 const Home = () => {
   return (
     <div className="home-container">
       <NavContainer />
       <Layout>
         <PageHeader heading="Home" headerType="headingOnly" />
-        <HomePageContainer />
+        <SettingsCard/>
       </Layout>
     </div>
   );
 };
 
 export default Home;
-
