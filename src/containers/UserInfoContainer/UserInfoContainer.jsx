3cace16bb8c05d92ef48fe514cb74f3ad161d04f
Tomas Truyols <tomast25@hotmail.com>
2023-02-25 00:07:09 +0000

    feat: added date picker and styled it according to the original calendar on figma

--- src/containers/UserInfoContainer/UserInfoContainer.jsx
+++ src/containers/UserInfoContainer/UserInfoContainer.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import DatePicker from "react-date-picker";
 import Button from "../../components/Button/Button";
 import DropdownField from "../../components/DropdownField/DropdownField";
 import InputField from "../../components/InputField/InputField";
+
 import "./UserInfoContainer.scss";
 
 const UserInfoContainer = () => {
@@ -42,9 +44,29 @@ const UserInfoContainer = () => {
         />
         <Button buttonText="Save" handleSubmit={handleSubmit} />
       </div>
-      <Calendar onChange={setDay} value={day} />
-      <div className="input-field-container__date-dropdown">
-        <DropdownField id="date" label="Date:" handleInput={handleDropdown} />
+      <div className="input-field-container__date-picker">
+        <DatePicker
+          onChange={setDay}
+          value={day}
+          format={"d/M/y"}
+          minDate={new Date()}
+          dayPlaceholder={"dd"}
+          monthPlaceholder={"mm"}
+          yearPlaceholder={"yyyy"}
+          maxDate={new Date("2026/12/31")}
+          minDetail={"decade"}
+          required
+        />
+      </div>
+      <div className="input-field-container__calendar">
+        <Calendar
+          onChange={setDay}
+          value={day}
+          minDate={new Date()}
+          maxDate={new Date("2026/12/31")}
+          minDetail={"decade"}
+          required
+        />
       </div>
       <div className="input-field-container__time-dropdown">
         <DropdownField id="time" label="Time:" handleInput={handleDropdown} />
