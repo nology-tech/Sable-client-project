0a3964fed48a8dadc2a83ae205e8c58cbb05cf58
navina321 <81630548+navina321@users.noreply.github.com>
2023-02-27 14:14:35 +0000

    added test

--- src/pages/Settings/Settings.test.js
+++ src/pages/Settings/Settings.test.js
@@ -17,3 +17,36 @@ it("should render the log out button on the settings page", () => {
     // assert
     expect(logOutButton).toBeInTheDocument();
 });
+
+it("should not show the nav bar on the mobile view", () => {
+    // arrange
+    render(
+      <BrowserRouter>
+        <Settings />
+      </BrowserRouter>
+    );
+  
+    //act
+    window.matchMedia = jest.fn().mockImplementation((query) => ({
+      matches: query !== "(min-width: 240px) and (max-width: 767px)",
+    }));
+    const nav = screen.getByRole("navigation");
+  
+    //assert
+    expect(nav).toBeInTheDocument();
+});
+
+it("should render the home footer on the page in mobile view", () => {
+    //arrange
+    render(
+      <BrowserRouter>
+        <Settings />
+      </BrowserRouter>
+    );
+  
+    //act
+    const logOutButton = screen.getByRole('button', { name: /log out/i})
+  
+    // assert
+    expect(logOutButton).toBeInTheDocument();
+});
