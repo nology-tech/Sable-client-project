a2446c75ea27b10ad2b7cc0bf8239cf859b7d829
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-02-27 16:46:52 +0000

    added link functionality

--- src/components/HomepageCard/HomepageCard.jsx
+++ src/components/HomepageCard/HomepageCard.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./HomepageCard.scss";
 import Button from "../Button/Button";
-
-const HomepageCard = ({ heading }) => {
+import { Link } from "react-router-dom";
+const HomepageCard = ({ heading, link }) => {
   return (
     <main className="homepage-card">
       <h2 className="homepage-card__heading">{heading}</h2>
       <div className="homepage-card__button">
-        <Button buttonText={"Go"} />
+        <Link to={`/${link}`} ><Button buttonText={"Go"}/> </Link>
       </div>
-      <div className="homepage-card__arrow">
-      </div>
+      <Link to={`/${link}`}><div className="homepage-card__arrow">  </div></Link>
+     
     </main>
   );
 };
--- src/containers/HomePageContainer/HomePageContainer.jsx
+++ src/containers/HomePageContainer/HomePageContainer.jsx
@@ -4,37 +4,50 @@ import HomepageCard from "../../components/HomepageCard/HomepageCard";
 import Button from "../../components/Button/Button.jsx";
 
 const HomePageContainer = () => {
+
+
   return (
     <main className="homepage-content">
       <section className="homepage-container">
         <div className="homepage-container__new-appointment">
-          <HomepageCard
+           <HomepageCard
             className="homepage-container__card"
             heading={"New Appointment"}
+            link="booking"
+
+            
           />
         </div>
         <div className="homepage-container__staff-details">
           <HomepageCard
             className="homepage-container__card"
             heading={"Staff Details"}
+            link="staff"
+
+           
           />
         </div>
         <div className="homepage-container__client-details">
           <HomepageCard
             className="homepage-container__card"
             heading={"Client Details"}
+            link="client"
           />
         </div>
         <div className="homepage-container__resources">
           <HomepageCard
             className="homepage-container__card"
             heading={"Resources"}
+            link="resources"
           />
         </div>
         <div className="homepage-container__settings">
           <HomepageCard
             className="homepage-container__card"
             heading={"Settings"}
+            link="settings"
+
+         
           />
         </div>
       </section>
