8dbce1968660c87d38c1a4a882e380e02a49a796
Hannah <hannahamitchell99@gmail.com>
2023-02-27 12:37:10 +0200

    removed button import

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import Button from "../Button/Button";
 import DropdownField from "../DropdownField/DropdownField";
 import InputField from "../InputField/InputField";
 import "./Form.scss";
