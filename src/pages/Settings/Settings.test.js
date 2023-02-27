57663af1333de9f0f89ebcf869aae824e711f604
Josh Tucker <joshuaatucker@hotmail.com>
2023-02-27 14:09:00 +0000

    started testing for settings card

--- src/pages/Settings/Settings.test.js
+++ src/pages/Settings/Settings.test.js
@@ -1,24 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { BrowserRouter } from "react-router-dom";
-import App from "../../App";
-import SettingsCard from "../../components/SettingsCard/SettingsCard";
 import Settings from "./Settings";
+import { BrowserRouter } from "react-router-dom";
 
-it("should render the apply button on the settings card on the page", () => {
-    //arrange
-    render(
-      <BrowserRouter>
-        <Settings />
-      </BrowserRouter>
-    );
-  
-    //act
-    const applyButton = screen.getByRole('button', { name: /apply/i})
-  
-    // assert
-    expect(applyButton).toBeInTheDocument();
-});
 
 it("should render the log out button on the settings page", () => {
     //arrange
@@ -34,37 +17,3 @@ it("should render the log out button on the settings page", () => {
     // assert
     expect(logOutButton).toBeInTheDocument();
 });
-
-it("the radio button should be selected when clicked", () => {
-    //arrange
-    render(
-      <BrowserRouter>
-        <Settings />
-      </BrowserRouter>
-    );
-  
-    //act
-    userEvent.click(screen.getByRole('radio', {name: /tech/i}))
-});
-
-it("the toggle button should slide when clicked", () => {
-    //arrange
-    render(
-      <BrowserRouter>
-        <Settings />
-      </BrowserRouter>
-    );
-  
-    //act
-    const toggle = screen.getByRole('checkbox')
-    userEvent.click(toggle)
-
-    //assert
-    expect(toggle.classList.contains("input:checked")).toBe(false)
-});
-
-
-
-
-
-
