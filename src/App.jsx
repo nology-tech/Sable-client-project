41c0fdc21cf1bf0ca255242d91373d1483466f01
Becky Rushton <beckyrushton30@gmail.com>
2023-02-28 11:07:05 +0000

    added turnary to app for web access function

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
 import Client from "./pages/Client/Client";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login.jsx";
-import Resources from "./pages/Resources/Resources.jsx"
+import Resources from "./pages/Resources/Resources.jsx";
 import NewAppointment from "./pages/NewAppointment/NewAppointment";
 import Staff from "./pages/Staff/Staff.jsx";
 import "./styles/base/_reset.scss";
 import "./styles/base/_typography.scss";
 
 const App = () => {
+  const [user, setUser] = useState(false);
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/booking" element={<NewAppointment />} />
-      <Route path="/staff" element={<Staff />} />
-      <Route path="/client" element={<Client />} />
-      <Route path="/resources" element={<Resources />} />
+      {!user ? (
+        <>
+          <Route
+            path="/"
+            element={
+              <>
+                <Login user={user} setUser={setUser} />{" "}
+                <Navigate replace to="/" />{" "}
+              </>
+            }
+          />
+        </>
+      ) : (
+        <>
+          <Route path="/home" element={<Home />} />
+          <Route path="/booking" element={<NewAppointment />} />
+          <Route path="/staff" element={<Staff />} />
+          <Route path="/client" element={<Client />} />
+          <Route path="/resources" element={<Resources />} />
+        </>
+      )}
     </Routes>
   );
 };
