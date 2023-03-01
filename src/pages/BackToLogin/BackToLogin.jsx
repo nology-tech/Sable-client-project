5f5a095982568144fefcfd171222b60fe541eb26
Becky Rushton <beckyrushton30@gmail.com>
2023-03-01 16:19:35 +0000

    added styling to page header

--- src/pages/BackToLogin/BackToLogin.jsx
+++ src/pages/BackToLogin/BackToLogin.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import "./BackToLogin.scss";
 import ConfirmationOverlay from "../../components/ConfirmationOverlay/ConfirmationOverlay";
 import Layout from "../../components/Layout/Layout";
+import PageHeader from "../../components/PageHeader/PageHeader";
 
 const BackToLogin = () => {
   return (
     <Layout>
-      <ConfirmationOverlay
-        overlayType="overlayLogin"
-        text="Logged out!"
-        buttonStyleOne="isMobilePopup"
-        buttonTextOne="Back to login"
-      />
+      <div className="header-container">
+        <PageHeader heading="" headerType="headingOnly" />
+      </div>
+
+      <div className="content-container">
+        <ConfirmationOverlay
+          overlayType="overlayLogin"
+          text="Logged out!"
+          buttonStyleOne="isMobilePopup"
+          buttonTextOne="Back to login"
+        />
+      </div>
     </Layout>
   );
 };
