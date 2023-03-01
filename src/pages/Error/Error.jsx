e6574c4ed7bb6569c958621643425f51e6a09ad5
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-03-01 09:27:33 +0000

    test: added tests for error page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Error.scss";
-const Error = ({page}) => {
+const Error = ({ page }) => {
   return (
     <div className="error-page">
       <main className="error-page__main">
@@ -9,9 +9,7 @@ const Error = ({page}) => {
           <span className="error-page__bg">Error - 401</span>
         </h1>
         <p>{`You have tried to access ${page} page without logging in, to continue:`}</p>
-        <p>
-          * Return to our homepage:
-        </p>
+        <p>* Return to our homepage:</p>
         <nav className="error-page__nav">
           <Link to="/">Login</Link>
         </nav>
