1509e851e71dae9522771be8da7875d7bfd27b13
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-03-01 11:54:44 +0000

    Added submit functionality

--- src/containers/BookingOverview/BookingOverview.jsx
+++ src/containers/BookingOverview/BookingOverview.jsx
@@ -5,41 +5,85 @@ import DatePicker from "react-date-picker";
 import Button from "../../components/Button/Button";
 import DropdownField from "../../components/DropdownField/DropdownField";
 import InputField from "../../components/InputField/InputField";
-
+import mockData from "../../data/mockData"
 import "./BookingOverview.scss";
-
 const UserInfoContainer = () => {
   const [day, setDay] = useState(new Date());
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [staff, setStaff] = useState("Stefania");
+  const [time, setTime] = useState("09:00");
+  const time_slots =  ['09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30',
+  '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30',
+  '17:00']
 
-  const handleName = () => {};
+  const handleInput = (event) => {
+    switch (event.target.id){
+      case "first-name":
+        setFirstName(event.target.value);
+        break;
+      
+      case "last-name": 
+        setLastName(event.target.value);
+        break; 
+      
+      case "email": 
+        setEmail(event.target.value);
+        break;
 
-  const handleEmail = () => {};
+      case "number":
+        setNumber(event.target.value);
+        break;
 
-  const handleNumber = () => {};
+      case "staff":
+        setStaff(event.target.value); 
+        break;
 
-  const handleDropdown = () => {};
+      case "time":
+        setTime(event.target.value);
+        break; 
+    }
+  }
 
-  const handleSubmit = () => {};
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    const objectToAdd = {
+      client: (firstName + " " + lastName),
+      bookingDate: day,
+      bookingTime: time,
+      confirmed: true,
+      Staff: staff,
+      email: email, 
+      number: number,
+      
+    }
+    mockData.bookings.push(objectToAdd)
+  };
 
   return (
     <form className="input-field-container">
       <div className="input-field-container__firstName">
-        <InputField id="first-name" label="First Name" handleInput={handleName} />
+        <InputField id="first-name" label="First Name" handleInput={handleInput} />
       </div>
       <div className="input-field-container__lastName">
-        <InputField id="last-name" label="Last Name" handleInput={handleName} />
+        <InputField id="last-name" label="Last Name" handleInput={handleInput} />
       </div>
       <div className="input-field-container__email">
-        <InputField id="email" label="Email Address" handleInput={handleEmail} />
+        <InputField id="email" label="Email Address" handleInput={handleInput} />
       </div>
       <div className="input-field-container__mobile">
-        <InputField id="number" label="Mobile Number" handleInput={handleNumber} />
+        <InputField id="number" label="Mobile Number" handleInput={handleInput} />
       </div>
       <div className="input-field-container__staff-dropdown">
         <DropdownField
           id="staff"
           label="Staff Member"
-          handleInput={handleDropdown}
+          handleInput={handleInput}
+          options={mockData.staff.map((staffMember) => {
+            return staffMember.name; 
+          })}
         />
       </div>
       <div id="datePicker" className="input-field-container__date-picker">
@@ -73,22 +117,22 @@ const UserInfoContainer = () => {
         />
       </div>
       <div className="input-field-container__time-dropdown">
-        <DropdownField id="time" label="Time" handleInput={handleDropdown} />
+        <DropdownField id="time" label="Time" handleInput={handleInput} options={time_slots}/>
       </div>
       <div className="input-field-container__submit">
         <Button
           buttonText="Submit"
-          handleSubmit={handleSubmit}
+          handleClick={handleSubmit}
           buttonStyle="isMobileDefault"
         />
       </div>
       <div className="input-field-container__buttons">
         <Button
           buttonText="Cancel"
-          handleSubmit={handleSubmit}
+          handleClick={handleSubmit}
           buttonStyle="isCancel--grey"
         />
-        <Button buttonText="Save" handleSubmit={handleSubmit} />
+        <Button buttonText="Save" handleClick={handleSubmit} />
       </div>
     </form>
   );
