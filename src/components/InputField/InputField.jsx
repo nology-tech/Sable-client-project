95665be030c52ae33c26c5e2a60be1937d37c8f0
DexterFreeman <67049776+DexterFreeman@users.noreply.github.com>
2023-03-02 10:27:00 +0000

    changed InputField

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./InputField.scss"
 
-const InputField = ({label, placeholderText, id, handleInput, readOnly, currentValue}) => {
+const InputField = ({label, placeholderText, id, handleInput, readOnly, editValue}) => {
   return (
     <div className='input-field'>
         <label className='input-field__label' htmlFor={id}>{label}</label>
-        <input className="input-field__input" readOnly={readOnly} id={id} type="text" placeholder={placeholderText} value={currentValue} onChange={handleInput}/>
+        <input className="input-field__input" readOnly={readOnly} id={id} type="text" placeholder={placeholderText} onChange={handleInput} defaultValue={editValue}/>
     </div>
   )
 }
