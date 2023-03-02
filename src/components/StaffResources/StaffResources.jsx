c44544d0a9f810a1764be85fe28b7e49772a9ebb
TristanP <tristanpere@googlemail.com>
2023-03-02 22:12:21 +0000

    feature: Resource Page updated to show correct resource requests as well as staff requesting them

--- src/components/StaffResources/StaffResources.jsx
+++ src/components/StaffResources/StaffResources.jsx
@@ -3,7 +3,7 @@ import "./StaffResources.scss";
 import DataCard from "../DataCard/DataCard";
 //import resourceData from "../../mockData.js"
 
-const StaffResources = ({ staffName, renderLabels, cardsArray }) => {
+const StaffResources = ({ staffName, cardsArray }) => {
   const cardJSX = cardsArray.map((object, index) => {
     return (
       <div key={index} className="staff-resources__cards-card">
@@ -15,16 +15,6 @@ const StaffResources = ({ staffName, renderLabels, cardsArray }) => {
   return (
     <div className="staff-resources">
       <h1 className="staff-resources__staff-member">{staffName}</h1>
-      {renderLabels && (
-        <div className="staff-resources__labels">
-          <p className="booking-container__label">Resource Name</p>
-          <p className="booking-container__label">Quantity Remaining</p>
-          <p className="booking-container__label">Cost per unit</p>
-          <p className="booking-container__label">Auto-Purchase</p>
-          <p className="booking-container__label">Auto-Purchase Level</p>
-          <p className="booking-container__label">Quantity Requested</p>
-        </div>
-      )}
       <div className="staff-resources__cards">{cardJSX && cardJSX}</div>
     </div>
   );
--- src/containers/StaffResourcesContainer/StaffResourcesContainer.jsx
+++ src/containers/StaffResourcesContainer/StaffResourcesContainer.jsx
@@ -1,16 +1,49 @@
 import React from "react";
 import "./StaffResourcesContainer.scss";
 import StaffResources from "../../components/StaffResources/StaffResources";
-import { mockData } from "../../data/mockData"
-const StaffResourcesContainer = () => {
+import { mockData } from "../../data/mockData";
+const StaffResourcesContainer = ({ isAdmin /*resourcesRequestArr*/ }) => {
+  const staffResourceList = (isAdmin) => {
+    let category = "";
+    isAdmin ? (category = "Admin") : (category = "Health");
+    const arrayOfStaff = mockData.staff.map((staff) => {
+      const staffResources = mockData.resourcesRequest.filter(
+        //rRA goes in here
+        (resourcesRequest) => {
+          return (
+            resourcesRequest.staffMember == staff.name &&
+            resourcesRequest.category == category
+          );
+        }
+      );
+      return [staff.name, staffResources];
+    });
+    const staffRequests = arrayOfStaff.filter((staff) => {
+      //when there is no match then it doesn't display
+      return staff[1].length != 0;
+    });
+    return staffRequests;
+  };
+
+  console.log(staffResourceList(isAdmin));
+
+  const staffJSX = staffResourceList(isAdmin).map((staff, index) => {
+    return (
+      <StaffResources key={index} staffName={staff[0]} cardsArray={staff[1]} />
+    );
+  });
+
   return (
     <div className="staff-resource-container">
-      <StaffResources
-        staffName={"Staff 01"}
-        renderLabels={true}
-        cardsArray={mockData.resources}
-      />
-      <StaffResources staffName={"Staff 02"} cardsArray={mockData.resources} />
+      <div className="staff-resources__labels">
+        <p className="booking-container__label">Resource Name</p>
+        <p className="booking-container__label">Quantity Remaining</p>
+        <p className="booking-container__label">Cost per unit</p>
+        <p className="booking-container__label">Auto-Purchase</p>
+        <p className="booking-container__label">Auto-Purchase Level</p>
+        <p className="booking-container__label">Quantity Requested</p>
+      </div>
+      {staffJSX}
     </div>
   );
 };
